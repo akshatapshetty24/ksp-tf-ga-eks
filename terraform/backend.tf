dd3477d7629adb1a700a50ebf3e60d5cba425304
akshatapshetty24 <akshatapshetty24@gmail.com>
2025-05-15 19:13:22 +0530

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "ksp-tf-ga-eks-state-bucket"
-    key            = "eks/github-actions/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "ksp-tf-ga-eks-lock-table"
-    encrypt        = true
-  }
-}
