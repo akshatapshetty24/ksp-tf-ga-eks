2dbbc104d57782318f8c67810f26f4aa263813e5
akshatapshetty24 <akshatapshetty24@gmail.com>
2025-05-15 18:53:26 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {}
+variable "cluster_name" {}
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+variable "public_subnet_cidrs" {
+  type = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
