42e8f4c4e473b5abd0ac79aa8b7fdd2aa0582754
akshatapshetty24 <akshatapshetty24@gmail.com>
2025-05-15 21:28:17 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "public_subnet_ids" {
 output "eks_cluster_endpoint" {
   value = aws_eks_cluster.example.endpoint
 }
+
+output "eks_node_group_name" {
+  value = aws_eks_node_group.example_nodes.node_group_name
+}
