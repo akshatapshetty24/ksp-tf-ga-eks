17f37436fb76ee9f29d411327ac620266b5b4dd7
akshatapshetty24 <akshatapshetty24@gmail.com>
2025-05-15 21:27:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,6 +4,18 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 variable "public_subnet_cidrs" {
-  type = list(string)
+  type    = list(string)
   default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+variable "node_instance_type" {
+  default = "t3.medium"
+}
+variable "desired_capacity" {
+  default = 2
+}
+variable "max_capacity" {
+  default = 3
+}
+variable "min_capacity" {
+  default = 1
 }
